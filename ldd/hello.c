709322809aeebb4ba39efbdddafbc103a0138032
Ragavendra <raga@example.com>
2023-09-12 19:32:57 -0700

    rvrd dtpu xunil

--- ldd/hello.c
+++ ldd/hello.c
@@ -4,13 +4,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	 printk(KERN_ALERT, "Hello, world\n");
+	 printk(KERN_ALERT "Hello, world\n");
 	 return 0;
 }
 
 static void hello_exit(void)
 {
-	 printk(KERN_ALERT, "Goodbye, world\n");
+	 printk(KERN_ALERT "Goodbye, world\n");
 }
 
 module_init(hello_init);
