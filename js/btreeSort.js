b21eca6c39324fc822ef0194a0a637bbf6fe2d74
Ragavendra <ragavendra.bn@gmail.com>
2024-12-04 17:14:29 -0800

    adding bst sort alg

--- js/btreeSort.js
+++ js/btreeSort.js
@@ -8,8 +8,7 @@ class Node {
 	} 
 } 
 
-/* A recursive function to 
-insert a new key in BST */
+/* insert a new key in BST */
 function insertBinaryNode(root, key) { 
 
 	/* If the tree is empty, 
@@ -29,22 +28,20 @@ return a new node */
 	return root; 
 } 
 
-// A function to do 
 // inorder traversal of BST - LNR
 function inorderTraverse(root) { 
 	if (root != null) { 
-		inorderRec(root.left); 
+		inorderTraverse(root.left); 
 		console.log(root.key + " "); 
-		inorderRec(root.right); 
+		inorderTraverse(root.right); 
 	} 
 } 
 
 // Root of BST 
-let root = new Node(); 
-root = null; 
+let root = null;
 
 // let arr = [5, 4, 7, 2, 11]; 
-let arr = [8, 7, 6 ,5 , 4, 3, 2, 1]
+let arr = [10, 11, 8, 7, 6 ,5 , 4, 3, 2, 1]
 
 // bst insertion
 for (let i = 0; i < arr.length; i++) { 
@@ -56,10 +53,8 @@ inorderTraverse(root);
 
 /* 
 total time complexity of O(n log n) in the average case, and O(n^2) in the worst case
-
 Finally, the worst-case time complexity of sorting a binary tree using the steps of the tree sort algorithm is as follows:
 T(SortingBinaryTree) =  T(BST Insertion) +  T(BST Traversal) = O(n^2) + O(n) = O(n^2) */
 
-// insert a key is done in O(n) in the worst case, and O(log n) in avg case
 // This code is contributed 
 // by Saurabh Jaiswal 
