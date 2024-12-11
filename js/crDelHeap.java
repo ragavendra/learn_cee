587bf5a2471d4125b21d7c7e82a9c21e2106c2cf
Ragavendra <ragavendra.bn@gmail.com>
2024-12-10 16:57:35 -0800

    updating heap to one

--- js/crDelHeap.java
+++ js/crDelHeap.java
@@ -6,45 +6,8 @@ public class delRootHeap {
     public static LinkedList<Integer> outPut = new LinkedList();
     // ([ 1, 2, 1, 6, 3, 4, 3 ]);
 
-    public static void delRootEle() {
-
-        int last = outPut.removeLast();
-
-        // update first or root to last
-        outPut.set(0, last);
-
-		heapify(0);
-
-        int nodePos = 0;
-
-		/*
-		 * 
-        while (nodePos != 0) {
-            int lefPos = (2 * nodePos) + 1;
-            int righPos = (2 * nodePos) + 2;
-            int leftChild = outPut.get(lefPos);
-            int rightChild = outPut.get(righPos);
-
-            // if node more than left, swap
-            if (outPut.get(nodePos) > leftChild) {
-                int temp = outPut.get(nodePos);
-                outPut.set(nodePos, leftChild);
-                outPut.set(lefPos, temp);
-                nodePos = lefPos;
-
-                // else if node more than right, swap
-            } else if (outPut.get(nodePos) > rightChild) {
-                int temp = outPut.get(nodePos);
-                outPut.set(nodePos, rightChild);
-                outPut.set(righPos, temp);
-                nodePos = righPos;
-            }
-        }
-		 */
-    }
-
 	// min heapify
-    public static void heapify(int index) {
+    public static void heapifyBelow(int index) {
         int leftChild = 2 * index + 1;
         int rightChild = 2 * index + 2;
 
@@ -54,7 +17,7 @@ public class delRootHeap {
             int temp = outPut.get(leftChild);
             outPut.set(leftChild, outPut.get(index));
             outPut.set(index, temp);
-            heapify(leftChild);
+            heapifyBelow(leftChild);
         }
 
         // System.out.println("L " + outPut.get(rightChild) + " < " + outPut.get(index));
@@ -63,15 +26,9 @@ public class delRootHeap {
             int temp = outPut.get(rightChild);
             outPut.set(rightChild, outPut.get(index));
             outPut.set(index, temp);
-            heapify(rightChild);
+            heapifyBelow(rightChild);
         }
     }
-    public static void PrintOutput() {
-		for(Integer ele: outPut){
-			System.out.println(ele);
-		}
-	}
-
     public static void RemoveEle(int pos) {
 		// remove from pos
         int last = outPut.removeLast();
@@ -79,26 +36,51 @@ public class delRootHeap {
         // update pos ele to last
         outPut.set(pos, last);
 
-		// parse inside the heap
-		heapify(pos);
-
-		// ArrayList<Integer> arr = new ArrayList();
-		// arr.remove(2);
+		// parse inside the heap or below it from that pos
+		heapifyBelow(pos);
 	}
 
-	public static Integer[] arrr;
+    public static void PrintOutput() {
+		for(Integer ele: outPut){
+			System.out.println(ele);
+		}
+	}
 
-    public static void main(String[] args) {
+    public static void insertEle(Integer ele) {
+
+        outPut.add(ele);
+
+        int lastIndex = outPut.size() - 1;
+        int parIndex;
+
+        while (lastIndex != 0) {
+
+			// k, 2k+1, 2k+2; 2k = l-1; k = (l-1)/2;
+            parIndex = (lastIndex - 1) / 2;
+
+			// if parent > child, swap with parent - min heap
+			if(outPut.get(parIndex) > ele) {
+				int par = outPut.set(parIndex, ele);
+				outPut.set(parIndex, ele);
+				outPut.set(lastIndex, par);
+			}
+
+			// Now loop till root is reached or par index is 0
+			// System.out.println("Value " + value + " Curr ind is " + curIndex + " Par ind is " + parentIndex);
+			lastIndex = parIndex;
+		}
+    }
+
+	// Remove ele
+    public static void main_(String[] args) {
 
 		// [10, 11, 8, 7, 6 ,5 , 4, 3, 2, 1]
-		Integer[] arr = { 1, 2, 1, 6, 3, 4, 3 };
+		Integer[] arr = { 2, 3, 4, 6, 1, 3, 1 };
+		// Integer[] arr = { 1, 2, 1, 6, 3, 4, 3 };
 		// Integer[] arr = { 85, 34, 16, 71, 57, 93, 28 };
 		// Integer[] arr = { 93, 57, 34, 85, 71 };
 		// Integer[] arr = { 85, 71, 57, 93 };
 		// Integer[] arr = { 93, 85, 71 };
-		// Integer[] arr = { 2, 3, 4, 6, 1 };
-
-		// Scanner scanner = new Scanner(System.in);
 
 		// fill list
 		for (Integer i = 0; i < arr.length; i++) {
@@ -107,21 +89,22 @@ public class delRootHeap {
 
 		RemoveEle(0);
 		RemoveEle(1);
-		// heapify(0);
+		// heapifyBelow(0);
 
         PrintOutput();
     }
 
-	/*
-	 * 
-    void deleteFromHeap(int index) {
-        System.out.println("Deleting the element " + outPut.get(index));
-        int lastIndex = outPut.size() - 1;
-        int temp = outPut.get(index);
-        outPut.set(index, outPut.get(lastIndex));
-        outPut.set(lastIndex, temp);
-        outPut.remove(lastIndex);
-        heapify(index);
+	// Create Heap/ Heapify
+    public static void main(String[] args) {
+
+		Integer[] arr = { 2, 3, 4, 6, 1, 3, 1 };
+
+		for (Integer i = 0; i < arr.length; i++) {
+            insertEle(arr[i]);
+        }
+
+		PrintOutput();
+		/* Exp op 1, 2, 1, 6, 3, 4, 3 */
     }
-	 */
+
 }
